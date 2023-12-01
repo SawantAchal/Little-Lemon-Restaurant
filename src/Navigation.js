8ce00f57e19908561c04ba5d2000b1fcc891398a
SawantAchal <achals1718@gmail.com>
2023-12-01 14:13:25 +0530

    did changes in navigation

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,23 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Assets/Navigation.css'
 import logo from './Assets/images/Logo .svg'
 import { Link } from 'react-router-dom'
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Navigation = () => {
+  const [menuOpen , setmenuOpen] = useState(false)
+
+  const toggleMenu = () =>{
+    setmenuOpen(!menuOpen);
+  }
+
   return (
     <>
-      <nav>
+      <nav className={`navbar ${menuOpen ? "open" : ""}`}>
         <img src={logo}/>
         <b>
-        <ul>
+        <ul className={`navbar-link ${menuOpen ? "visible" : ""}`}>
           <Link to="/"><li>Home</li></Link>
           <Link to="/"><li>About</li></Link>
           <Link to="/"><li>Menu</li></Link>
-          <Link to="/"><li>Reservation</li></Link>
-          <Link to="/"><li>Order online</li></Link>
+          <Link to="/"><li>Reservations</li></Link>
+          <Link to="/"><li>Order Online</li></Link>
           <Link to="/"><li>Login</li></Link>
         </ul>
         </b>
+        {/* Mobile Navigation */}
+        <div className='hamburger'onClick={toggleMenu} >
+            <GiHamburgerMenu  />
+        </div>
       </nav>
     </>
   )
