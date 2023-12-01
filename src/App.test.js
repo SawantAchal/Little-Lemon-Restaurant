6ff75d8c2d2a1b60c6c09a52fda7fa1de90d466b
SawantAchal <achals1718@gmail.com>
2023-12-01 12:49:41 +0530

    did chanfes in index.html , app.test.js src Hero.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,24 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
+import BookingForm from './Components/BookingForm';
+import { BrowserRouter } from 'react-router-dom';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders the Header heading ', () => {
+  render(<BrowserRouter><App /> </BrowserRouter>);
+  const headingElement = screen.getByText("Reserve table");
+  expect(headingElement).toBeInTheDocument();
+
+  const reservebutton = screen.getByRole("button")
+  fireEvent.click(reservebutton)
+
+  const headingElementNew = screen.getByText("Choose Date")
+  expect(headingElementNew).toBeInTheDocument()
 });
+
+test('Initialize/Update Times', () => { 
+  render (<BrowserRouter><App /> </BrowserRouter>);
+  const reservebutton = screen.getByRole("button")
+  fireEvent.click(reservebutton)
+
+  const testTime = []
+})
